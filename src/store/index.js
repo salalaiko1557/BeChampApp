758bf4c89053046fa29d34812d9743dab74c5afd
ALEX SALALAIKO <salalaiko1557@gmail.com>
2019-01-30 16:31:25 +0200

    Init commit

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state:{
+    tags:[
+      'a1', 'a2'
+    ]
+  },
+  getters:{
+
+  },
+  mutations:{
+
+  },
+  actions:{
+
+  }
+})
